2cda5c436f128a49a7114547cf4fc1dd71b13410
Eirik00 <eirik.rene@gmail.com>
2024-03-05 16:08:24 +0100

    router

---
+++ src/components/Infocard.jsx
@@ -0,0 +1,11 @@
+export default function Infocard({cardElement}){
+    return (
+        <article>
+            <h2></h2>
+            <p></p>
+            <ul>
+                <li></li>
+            </ul>
+        </article>
+    )
+}
